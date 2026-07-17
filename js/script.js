0cdb7f617f796cf4919d58c2c65a67444d966784
LauriH <lauri.hynonen@valuatum.com>
2026-07-17 15:54:29 +0300

    Restore Wisdom fresh report picker

--- js/script.js
+++ js/script.js
@@ -162,7 +162,7 @@ function handleSearch(query) {
     openCompanyPage(partial.url);
     return;
   }
-  window.location.href = `reports.html?search=${encodeURIComponent(query)}&source=homepage#coverage-request`;
+  window.location.href = `reports.html?search=${encodeURIComponent(query)}&source=homepage#order-fresh`;
 }
 
 function openCompanyPage(url) {
